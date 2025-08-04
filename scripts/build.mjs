9d954d6787795f8b2d368ac89fb7c49c1dd91e55
Cory Crowley <ccrowley96@gmail.com>
2025-08-04 09:13:59 -0400

    Basic TS project scaffolding

---
+++ scripts/build.mjs
@@ -0,0 +1,17 @@
+import { build } from 'esbuild'
+import { rm } from 'node:fs/promises'
+
+await rm('dist', { recursive: true, force: true })
+
+await build({
+  entryPoints: ['src/index.ts'],
+  outdir: 'dist',
+  bundle: true,
+  format: 'esm',
+  platform: 'neutral',
+  target: 'es2020',
+  minify: true,
+  sourcemap: true,
+})
+
+console.log('✨ esbuild complete')
